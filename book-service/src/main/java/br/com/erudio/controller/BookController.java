4762485d2e3226d36a31d73d7fe3318ba0679005
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-01-19 06:31:47 -0300

    Retornando Mocks e Testando o Book Service

---
+++ book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -0,0 +1,31 @@
+package br.com.erudio.controller;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Book;
+
+@RestController
+@RequestMapping(path = "/book-service")
+public class BookController {
+	
+	@Autowired
+	private Environment environment;
+
+	@GetMapping(path = "/{id}/{currency}")
+	public Book findBook(
+		@PathVariable(name = "id") Long id,
+		@PathVariable(name = "currency") String currency
+	) {
+		var port = environment.getProperty("local.server.port");
+		
+		return new Book(1L, "Nigel Poulton", "Docker Deep Dive", new Date(), Double.valueOf(13.7), currency, port);
+	}
+	
+}
